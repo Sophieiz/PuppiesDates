98b5a435da4819fd7d4981f2499608b7286f8d99
Sophieiz <ortegaobandoisabelsofia@gmail.com>
2026-07-22 23:09:18 -0500

    Agregar trim() a variables de entorno

--- PuppiesDate/src/java/Controlador/Conexion.java
+++ PuppiesDate/src/java/Controlador/Conexion.java
@@ -1,18 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Controlador;
 import java.sql.Connection;
 import java.sql.DriverManager;
 public class Conexion {
     private Connection conn;
     private String driver = "com.mysql.cj.jdbc.Driver";
-    private String user = System.getenv("DB_USER");
-    private String password = System.getenv("DB_PASSWORD");
-    private String baseDatos = System.getenv("DB_DATABASE");
-    private String host = System.getenv("DB_HOST");
-    private String port = System.getenv("DB_PORT");
+    private String user = System.getenv("DB_USER") != null ? System.getenv("DB_USER").trim() : null;
+    private String password = System.getenv("DB_PASSWORD") != null ? System.getenv("DB_PASSWORD").trim() : null;
+    private String baseDatos = System.getenv("DB_DATABASE") != null ? System.getenv("DB_DATABASE").trim() : null;
+    private String host = System.getenv("DB_HOST") != null ? System.getenv("DB_HOST").trim() : null;
+    private String port = System.getenv("DB_PORT") != null ? System.getenv("DB_PORT").trim() : null;
     private String url = "jdbc:mysql://" + host + ":" + port + "/" + baseDatos + "?useTimezone=true&serverTimezone=America/Bogota&useSSL=false&allowPublicKeyRetrieval=true";
      public Conexion() {
     conn = null;
